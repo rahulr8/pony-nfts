be3eff3a2fe13292d9f0d549b7b268c224797f20
Rahul <lolcode8@gmail.com>
2022-06-03 17:56:13 -0700

    Refactor into NftItem

---
+++ components/NftItem.tsx
@@ -0,0 +1,91 @@
+import { Box, Button, Flex, Image } from "@chakra-ui/react";
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+import { useState } from "react";
+
+interface NftItemProps {
+  nft: any;
+  nftCollection: any;
+}
+
+export const NftItem: React.FC<NftItemProps> = ({ nft, nftCollection }) => {
+  const [loading, setLoading] = useState(false);
+  const [selectedNFT, setSelectedNFT] = useState<number | null>(null);
+
+  // Function which generates signature and mints NFT
+  const mintNft = async (id: number) => {
+    setSelectedNFT(id);
+    setLoading(true);
+    connectWithMetamask;
+
+    try {
+      // Call API to generate signature and payload for minting
+      const response = await fetch("/api/get-pony-nfts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, address }),
+      });
+
+      if (response) {
+        connectWithMetamask;
+        const data = await response.json();
+        const mintInput = {
+          signature: data.signature,
+          payload: data.payload,
+        };
+
+        const mint = await nftCollection?.signature.mint(mintInput);
+        alert("NFT successfully minted!");
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log("error", error);
+      setLoading(false);
+      alert("Failed to mint NFT!");
+    }
+  };
+
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+
+  return (
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <Image width="30rem" height="15rem" src={nft?.url} alt="NFT image" />
+
+      <Flex p="1rem" alignItems="center" flexDir="column">
+        <Box
+          mt="1"
+          fontWeight="bold"
+          lineHeight="tight"
+          fontSize="20"
+          m="0.5rem"
+        >
+          {nft?.name}
+        </Box>
+
+        <Box fontSize="16" m="0.5rem">
+          {nft?.description}
+        </Box>
+        <Box fontSize="16" m="0.5rem">
+          {nft?.price}
+        </Box>
+        {loading ? (
+          selectedNFT === nft.id && (
+            <p>Mint in progress. Please approve transaction</p>
+          )
+        ) : nft?.minted ? (
+          <b>Someone already owns this pony</b>
+        ) : (
+          <Button
+            colorScheme="purple"
+            m="0.5rem"
+            onClick={() => mintNft(nft?.id)}
+          >
+            Mint pony!
+          </Button>
+        )}
+      </Flex>
+    </Box>
+  );
+};
--- components/Nfts.tsx
+++ components/Nfts.tsx
@@ -14,10 +14,10 @@ import {
   useChainId,
   ChainId,
 } from "@thirdweb-dev/react";
+import { NftItem } from "./NftItem";
 
 const Nfts = () => {
   const [loading, setLoading] = useState(false);
-  const [selectedNFT, setSelectedNFT] = useState<number | null>(null);
   const [nftMetadata, setNftMetadata] = useState([null]);
   const [fetchedNfts, setFetchedNfts] = useState(false);
 
@@ -45,44 +45,6 @@ const Nfts = () => {
   const nftCollectionAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS;
   const nftCollection = useNFTCollection(nftCollectionAddress);
 
-  // Function which generates signature and mints NFT
-  const mintNft = async (id: number) => {
-    setSelectedNFT(id);
-    setLoading(true);
-    connectWithMetamask;
-
-    try {
-      // Call API to generate signature and payload for minting
-      const response = await fetch("/api/get-pony-nfts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id, address }),
-      });
-
-      if (response) {
-        connectWithMetamask;
-        const data = await response.json();
-        const mintInput = {
-          signature: data.signature,
-          payload: data.payload,
-        };
-
-        const mint = await nftCollection?.signature.mint(mintInput);
-        alert("NFT successfully minted!");
-        setLoading(false);
-      }
-    } catch (error) {
-      console.log("error", error);
-      setLoading(false);
-      alert("Failed to mint NFT!");
-    }
-  };
-
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-
   const chainId = useChainId();
 
   if (chainId !== ChainId.Rinkeby) {
@@ -97,54 +59,11 @@ const Nfts = () => {
     return (
       <SimpleGrid m="2rem" justifyItems="center" columns={3} spacing={10}>
         {nftMetadata?.map((nft: any) => (
-          <Box
+          <NftItem
             key={nftMetadata.indexOf(nft)}
-            maxW="sm"
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-          >
-            <Image
-              width="30rem"
-              height="15rem"
-              src={nft?.url}
-              alt="NFT image"
-            />
-
-            <Flex p="1rem" alignItems="center" flexDir="column">
-              <Box
-                mt="1"
-                fontWeight="bold"
-                lineHeight="tight"
-                fontSize="20"
-                m="0.5rem"
-              >
-                {nft?.name}
-              </Box>
-
-              <Box fontSize="16" m="0.5rem">
-                {nft?.description}
-              </Box>
-              <Box fontSize="16" m="0.5rem">
-                {nft?.price}
-              </Box>
-              {loading ? (
-                selectedNFT === nft.id && (
-                  <p>Mint in progress. Please approve transaction</p>
-                )
-              ) : nft?.minted ? (
-                <b>Someone already owns this pony</b>
-              ) : (
-                <Button
-                  colorScheme="purple"
-                  m="0.5rem"
-                  onClick={() => mintNft(nft?.id)}
-                >
-                  Mint pony!
-                </Button>
-              )}
-            </Flex>
-          </Box>
+            nft={nft}
+            nftCollection={nftCollection}
+          />
         ))}
       </SimpleGrid>
     );
